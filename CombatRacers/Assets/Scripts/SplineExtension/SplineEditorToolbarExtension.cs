8c4055cc056ce02c158380ed9fc0227ce891f12a
Ahmed Alnahdi <111814199+RealMortal@users.noreply.github.com>
2025-06-03 23:57:43 +0300

    Update SplineEditorToolbarExtension.cs

--- CombatRacers/Assets/Scripts/SplineExtension/SplineEditorToolbarExtension.cs
+++ CombatRacers/Assets/Scripts/SplineExtension/SplineEditorToolbarExtension.cs
@@ -1,25 +1,28 @@
-/*
 using NUnit.Framework;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace UnityEditor.Splines
 {
+    // Static helper class for interacting with the spline selection in the editor
     public static class SplineEditorToolbarExtension
     {
+        // Returns true if there is an active spline selection, false otherwise
         public static bool HasSelection()
         {
             return SplineSelection.HasActiveSplineSelection();
         }
 
-
+        // Retrieves the currently selected spline elements as a list of SelectedSplineElementInfo structs
         public static List<SelectedSplineElementInfo> GetSelection()
         {
-
+            // Get the current selection list of selectable spline elements from the editor
             List<SelectableSplineElement> elements = SplineSelection.selection;
 
+            // Prepare a new list to hold the processed selection info
             List<SelectedSplineElementInfo> infos = new List<SelectedSplineElementInfo>();
 
+            // Iterate over each selectable spline element and convert it into SelectedSplineElementInfo
             foreach (SelectableSplineElement element in elements)
             {
                 infos.Add(new SelectedSplineElementInfo(element.target, element.targetIndex, element.knotIndex));
@@ -27,25 +30,21 @@ namespace UnityEditor.Splines
 
             return infos;
         }
-
     }
 
-   
-
+    // Struct representing the essential info about a selected spline element in the editor
     public struct SelectedSplineElementInfo
     {
-        public Object target;
-        public int knotIndex;
-        public int targetIndex;
-        
+        public Object target;      // The Unity Object that owns the spline (usually SplineContainer)
+        public int targetIndex;    // Index of the spline within the container
+        public int knotIndex;      // Index of the knot within the spline
 
-        public SelectedSplineElementInfo(Object Object,int index,int knot)
+        // Constructor to initialize the selected element info fields
+        public SelectedSplineElementInfo(Object target, int targetIndex, int knotIndex)
         {
-            target = Object;
-            targetIndex = index;
-            knotIndex = knot;
-
+            this.target = target;
+            this.targetIndex = targetIndex;
+            this.knotIndex = knotIndex;
         }
     }
 }
-*/
