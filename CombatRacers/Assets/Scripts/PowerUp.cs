46a4ee9a1c8c21e7d92957bfc68e9bdbb7bd37f7
Ahmed Alnahdi <111814199+RealMortal@users.noreply.github.com>
2025-06-03 23:51:07 +0300

    Update PowerUp.cs

--- CombatRacers/Assets/Scripts/PowerUp.cs
+++ CombatRacers/Assets/Scripts/PowerUp.cs
@@ -3,11 +3,17 @@ using UnityEngine.UI;
 
 public abstract class PowerUp : MonoBehaviour
 {
+    // Flag to check if the power-up has already been used
     public bool IsUsed;
+
+    // Optional callback action to invoke when the power-up is used
     public System.Action OnUse { get; set; }
 
     [Header("UI")]
-    public Texture iconTexture; 
+    // Icon texture to represent this power-up in the UI
+    public Texture iconTexture;
 
+    // Abstract method to define how the power-up activates for a given player
+    // Must be implemented by subclasses
     public abstract void ActivatePowerUp(GameObject player);
 }
